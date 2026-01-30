59dc2961559f9cf9051dfe9a0941cba4333b851b
Anil <anilbeeraka99@gmail.com>
2026-01-30 17:12:55 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shellscript"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "run this as root user" | tee -a $LOGS_FILE
+    exit 1
+fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2  is failure" | tee -a $LOGS_FILE
+    else
+        echo "$2  is success" | tee -a $LOGS_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongo repo..."
+
+dnf install mongodb-org -y 
+VALIDATE $? "installing monogdb server..."
+
+systemctl enable mongod 
+VALIDATE $? "Enable monogdb s..."
+
+systemctl start mongod 
+VALIDATE $? "start monogdb ..."
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "allowing remote connection ..."
+
+systemctl restart mongod
+VALIDATE $? "restarted mongod ..."
