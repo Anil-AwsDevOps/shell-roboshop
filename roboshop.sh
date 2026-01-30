dd2a6b7838b2b3a4a53b21be1edee648043c656b
Anil <anilbeeraka99@gmail.com>
2026-01-30 16:18:06 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -7,56 +7,56 @@ DOMAIN_NAME="anilbeeraka.online"
 
 
 for instance in $@
-do 
-    INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type "t3.micro" \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text )
-     if [ $instance == "frontend" ]; then
-        IP=$(
-            aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text
-        )
-        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
-    else
-        IP=$(
-            aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text
-        )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.anilbeeraka.online
-    fi
+    do 
+        INSTANCE_ID=$( aws ec2 run-instances \
+        --image-id $AMI_ID \
+        --instance-type "t3.micro" \
+        --security-group-ids $SG_ID \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text )
+        if [ $instance == "frontend" ]; then
+            IP=$(
+                aws ec2 describe-instances \
+                --instance-ids $INSTANCE_ID \
+                --query 'Reservations[].Instances[].PublicIpAddress' \
+                --output text
+            )
+            RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+        else
+            IP=$(
+                aws ec2 describe-instances \
+                --instance-ids $INSTANCE_ID \
+                --query 'Reservations[].Instances[].PrivateIpAddress' \
+                --output text
+            )
+            RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.anilbeeraka.online
+        fi
 
-    echo "IP Address: $IP"
+        echo "IP Address: $IP"
 
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record",
-        "Changes": [
-            {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$RECORD_NAME'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+        {
+            "Comment": "Updating record",
+            "Changes": [
                 {
-                    "Value": "'$IP'"
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                    {
+                        "Value": "'$IP'"
+                    }
+                    ]
                 }
-                ]
-            }
-            }
-        ]
-    }
-    '
+                }
+            ]
+        }
+        '
 
-    echo "record updated for $instance"
-done
+        echo "record updated for $instance"
+    done
